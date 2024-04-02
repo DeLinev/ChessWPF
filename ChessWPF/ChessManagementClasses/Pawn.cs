826149e6581bb2411a913b414fec5c4525bccb50
Denys_L <ipz231_ldyu@student.ztu.edu.ua>
2024-04-02 17:28:26 +0300

    Implement the possibility of alternate movement of pieces on the board. Fix the GetPossibleMoves() method for Pawn class

--- ChessWPF/ChessManagementClasses/Pawn.cs
+++ ChessWPF/ChessManagementClasses/Pawn.cs
@@ -18,24 +18,52 @@ namespace ChessManagementClasses
 		public override List<MoveBase> GetPossibleMoves(Board board, Position position)
 		{
 			List<MoveBase> moves = new List<MoveBase>();
+			Position current;
 
-			// Adds possible move for the Up direction
-			Position current = position.ChangePosition(PositionChanges.Up);
+			if (Color == PieceColor.White) // White pawns
+			{
+				// Adds possible move for the Up direction
+				current = position.ChangePosition(PositionChanges.Up);
+				if (Board.IsPositionValid(current) && board.IsPositionEmpty(current))
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
 
-			if (Board.IsPositionValid(current) && board.IsPositionEmpty(current))
-				moves.Add(new RegularMove(new Position(position), new Position(current)));
+				// Adds possible move for the Upx2 direction
+				current = position.ChangePosition(PositionChanges.Up, 2);
+				if (Board.IsPositionValid(current) && board.IsPositionEmpty(current) && !this.HasMoved)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
 
-			// Adds possible move for the UpLeft direction
-			current = position.ChangePosition(PositionChanges.UpLeft);
+				// Adds possible move for the UpLeft direction
+				current = position.ChangePosition(PositionChanges.UpLeft);
+				if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
 
-			if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
-				moves.Add(new RegularMove(new Position(position), new Position(current)));
+				// Adds possible move for the UpRight direction
+				current = position.ChangePosition(PositionChanges.UpRight);
+				if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
+			}
+			else // Black pawns
+			{
+				// Adds possible move for the Down direction
+				current = position.ChangePosition(PositionChanges.Down);
+				if (Board.IsPositionValid(current) && board.IsPositionEmpty(current))
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
 
-			// Adds possible move for the UpRight direction
-			current = position.ChangePosition(PositionChanges.UpRight);
+				// Adds possible move for the Downx2 direction
+				current = position.ChangePosition(PositionChanges.Down, 2);
+				if (Board.IsPositionValid(current) && board.IsPositionEmpty(current) && !this.HasMoved)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
 
-			if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
-				moves.Add(new RegularMove(new Position(position), new Position(current)));
+				// Adds possible move for the DownLeft direction
+				current = position.ChangePosition(PositionChanges.DownLeft);
+				if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
+
+				// Adds possible move for the DownRight direction
+				current = position.ChangePosition(PositionChanges.DownRight);
+				if (Board.IsPositionValid(current) && !board.IsPositionEmpty(current) && board.GetPiece(current).Color != Color)
+					moves.Add(new RegularMove(new Position(position), new Position(current)));
+			}
 
 			return moves;
 		}
--- ChessWPF/UserControls/GameUserControl.xaml.cs
+++ ChessWPF/UserControls/GameUserControl.xaml.cs
@@ -21,14 +21,18 @@ namespace ChessWPF.UserControls
 	/// </summary>
 	public partial class GameUserControl : UserControl
 	{
-		//protected Image[,] ChessPieces;
-		protected Board board = new Board();
+		protected Board board;
+		protected Shape[,] possibleMovesOverlay;
+		protected Position chosenPos;
+		protected List<MoveBase> possibleMoves;
 
 		public GameUserControl()
 		{
 			InitializeComponent();
 			MoveSideTextBlock.Text = "White to move";
-			//ChessPieces = new Image[8, 8];
+			board = new Board();
+			possibleMovesOverlay = new Shape[8, 8];
+			possibleMoves = new List<MoveBase>();
 			board.SetBoard();
 			DrawPieces();
 		}
@@ -136,8 +140,93 @@ namespace ChessWPF.UserControls
 					img.Source = bitmapImage;
 					img.Width = 60;
 					BoardGrid.Children.Add(img);
+
+					Ellipse ellipse = new();
+					possibleMovesOverlay[i, j] = ellipse;
+					//OverlayGrid.Children.Add(ellipse);
 				}
 			}
+		}
+
+		private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Grid grid = (Grid)sender;
+
+			Point mousePosition = e.GetPosition(grid);
+
+			if (mousePosition.X > BoardGrid.ActualWidth || mousePosition.Y < MoveSideTextBlock.ActualHeight)
+				return;
+
+			// Calculate the cell that was clicked
+			double cellHeight = BoardGrid.ActualHeight / 8;
+			double cellWidth = BoardGrid.ActualWidth / 8;
+			double actualYOfClick = mousePosition.Y - MoveSideTextBlock.ActualHeight;
+			double actualXOfClick = mousePosition.X;
+			int rank = (int)(actualYOfClick / cellHeight);
+			int file = (int)(actualXOfClick / cellWidth);
+
+			if (possibleMoves.Count == 0)
+				possibleMoves = board.GetPossibleMoves(new Position(rank, file));
+
+			if (chosenPos != null)
+			{
+				possibleMovesOverlay[chosenPos.currentRank, chosenPos.currentFile].Fill = Brushes.Transparent;
+				chosenPos = null;
+
+				foreach (var move in possibleMoves)
+				{
+					possibleMovesOverlay[move.EndPosition.currentRank, move.EndPosition.currentFile].Fill = Brushes.Transparent;
+
+					if (move.EndPosition.currentRank == rank && move.EndPosition.currentFile == file)
+					{
+						board.Move(move);
+						BoardGrid.Children.Clear();
+						DrawPieces();
+						MoveSideTextBlock.Text = board.CurrentPlayer == PieceColor.White ? "White to move" : "Black to move";
+						break;
+					}
+				}
+
+				OverlayGrid.Children.Clear();
+				possibleMoves.Clear();
+
+            }
+			else
+			{
+				if (board.GetPiece(new Position(rank, file)) != null && board.CurrentPlayer != board.GetPiece(new Position(rank, file)).Color)
+				{
+					possibleMoves.Clear();
+					return;
+				}
+
+				if (possibleMoves.Count > 0)
+				{
+					chosenPos = new Position(rank, file);
+					
+					Color col = Color.FromRgb(153, 145, 236);
+
+					foreach (var move in possibleMoves)
+					{
+						possibleMovesOverlay[move.EndPosition.currentRank, move.EndPosition.currentFile].Fill = new SolidColorBrush(col);
+						possibleMovesOverlay[move.EndPosition.currentRank, move.EndPosition.currentFile].Height = 20;
+						possibleMovesOverlay[move.EndPosition.currentRank, move.EndPosition.currentFile].Width = 20;
+					}
+
+					possibleMovesOverlay[rank, file] = new Rectangle();
+					possibleMovesOverlay[rank, file].Fill = new SolidColorBrush(col);
+
+					AddToOverlayGrid();
+				}
+			}
+		}
+
+		protected void AddToOverlayGrid()
+		{
+			OverlayGrid.Children.Clear();
+
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+					OverlayGrid.Children.Add(possibleMovesOverlay[i, j]);
 		}
 	}
 }
